b9de50983403080f339c81f774c34d32ac69dc5e
Álvaro Catalán <alvarocatalans98@gmail.com>
2026-06-22 22:17:55 -0400

    area de rectangulo

--- Python/area.py
+++ Python/area.py
@@ -1,10 +1,16 @@
-base = input("Ingresar la base del rectangulo : ")
-altura = input("Ingrese la altura del rectangulo :")
+print()
+print(" calcular el area de un rectángulo")
+print(" ---------------------------------\n")# el \n es un salto de liena
+
+
+print()
+base = input("Ingresar la base del rectángulo : ")
+altura = input("Ingrese la altura del rectángulo :")
 
 #        enetros
 if base.isdigit() and altura.isdigit() :
     area = int(base) * int(altura) 
-    print(f"El area del rectangulo es: {area}")
+    print(f"El area del rectángulo es: {area}")
 else:
     print("ERROR")
     print("Los valores ingresados deben ser numeros enteros") 
